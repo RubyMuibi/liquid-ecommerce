96aea724ff4b545073824ee457205b2e8c3a683b
Ruby Muibi <ruby@rubymuibi.com>
2024-01-03 17:45:50 -0500

    update

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,15 @@
-import './globals.css'
-
 import { Inter } from 'next/font/google'
-
-
-
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: "Liquid",
-  description: "Liquid: e-commerce",
-}
-
+ 
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
 export default function RootLayout({ children }) {
   return (
-    
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
+    </html>
   )
 }
